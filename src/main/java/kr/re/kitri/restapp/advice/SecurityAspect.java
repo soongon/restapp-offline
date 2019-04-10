e3a2837da6c5f7c09665fef4af56b6d8fd5bd653
KITRI-25 <soongon@gmail.com>
2019-04-10 15:07:12 +0900

    AOP 토큰인증 완료

--- src/main/java/kr/re/kitri/restapp/advice/SecurityAspect.java
+++ src/main/java/kr/re/kitri/restapp/advice/SecurityAspect.java
@@ -1,9 +1,18 @@
 package kr.re.kitri.restapp.advice;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import kr.re.kitri.restapp.annotation.TokenRequired;
+import kr.re.kitri.restapp.service.SecurityServiceImpl;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.DatatypeConverter;
 
 @Aspect
 @Component
@@ -11,6 +20,22 @@ public class SecurityAspect {
 
     @Before("@annotation(tokenRequired)")
     public void authWithToken(TokenRequired tokenRequired) {
-        System.out.println("annotation with token...............");
+        ServletRequestAttributes reqAttributes =
+                (ServletRequestAttributes)
+                        RequestContextHolder.currentRequestAttributes();
+        HttpServletRequest request = reqAttributes.getRequest();
+        // checks for token in request header
+        String tokenInHeader = request.getHeader("token");
+        if(StringUtils.isEmpty(tokenInHeader)){
+            throw new IllegalArgumentException("Empty token");
+        }
+        Claims claims = Jwts.parser().setSigningKey(
+                DatatypeConverter.parseBase64Binary(
+                        SecurityServiceImpl.secretKey))
+                .parseClaimsJws(tokenInHeader).getBody();
+        if(claims == null || claims.getSubject() == null) {
+            throw new IllegalArgumentException("Token Error : Claim is null");
+        }
+        System.out.println("토큰에 포함된 subject로 자체 인증처리 필요..");
     }
 }
